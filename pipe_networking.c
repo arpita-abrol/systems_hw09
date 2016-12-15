b56aca70352c3eaa41b56a7aaf127bec23450397
Arpita <aabrol@stuy.edu>
2016-12-15 18:59:06 -0500

    add initial files

---
+++ pipe_networking.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int client_handshake(int *to_client) {
+
+}
+
+int server_handshake(int *from_client) {
+
+}
+
+int main() {
+
+  return 0;
+}
---
+++ pipe_networking.h
@@ -0,0 +1,4 @@
+#define MESSAGE_BUFFER_SIZE 1024
+
+int client_handshake(int *to_client);
+int server_handshake(int *from_client);
